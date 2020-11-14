460c7afa4feb7fe8cee3729ba903797f8f20677c
nijepa <nijepa@gmail.com>
2020-11-14 17:46:04 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
+<!--     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+  @import './assets/css/app.css';
+  
+  #app {
+    @import url('https://fonts.googleapis.com/css2?family=Play&display=swap');
+    // font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Play', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
